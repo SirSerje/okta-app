805875c097fa069b71c4006802ea584a525304a3
SirSerje <sir.serje@gmail.com>
2018-09-29 14:48:50 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// src/App.js
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, ImplicitCallback } from '@okta/okta-react';
+import Home from './Home';
+
+const config = {
+  issuer: 'https://dev-833658.oktapreview.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '{clientId}'
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Security issuer={config.issuer}
+                  client_id={config.client_id}
+                  redirect_uri={config.redirect_uri}
+        >
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/implicit/callback' component={ImplicitCallback}/>
+        </Security>
+      </Router>
+    );
+  }
+}
+
+export default App;
